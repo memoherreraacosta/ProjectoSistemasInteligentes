a55e74c5cdaba399cc573814ca2c15e26ffd1335
RichiePalma <palmamendoza.5@hotmail.com>
2020-09-18 00:48:45 -0500

    Sampling indexes implemented
    Signed-off-by: RichiePalma <palmamendoza.5@hotmail.com>

--- Actividades/Problemas de clasificación/problem_3.py
+++ Actividades/Problemas de clasificación/problem_3.py
@@ -27,28 +27,78 @@ from glob import glob
 #     i = i + 1
 #     print(i)
 
-files = glob("*.txt")
-with open(files[0], "r") as f:
-    data = [
-        register.split()[1:]
-        for register
-        in f.readlines()
-    ]
+category_1 = []
+category_2 = []
+# x = [:,1:] datos
+# y = [:,0] etiquetas
+# x.shape(dimension) algo asi
 
-print(data)
+datos = np.loadtxt("Datos misteriosos.txt",dtype="float")
+x = datos[:,1:]
+y = datos[:,0]
+targets = ["Clase1","Clase2"]
+n_clases = len(targets)
+
+features = [str(i) for i in range(len(x[0]))]
+n_features = len(x[0])
+
+print("len x",len(x))
+print("len y", len(y))
+# print(x)
+
+percentage = 0.1
+sample_size = round(len(x) * percentage)
+sample_indexes = np.random.choice(len(x),sample_size,replace=False)
+print("sample size",sample_size,"(",percentage*100,"% of total data )")
+print("indexes len",len(sample_indexes))
+print("indexes",sample_indexes)
+
+temp_x = []
+temp_y = []
+for index in sample_indexes:
+    temp_x.append(x[index])
+    temp_y.append(y[index])
+
+# print("temp x",temp_x[0])
+# print("temp y",temp_y)
+
+x = temp_x[:]
+y = temp_y[:]
+
+print("y sample",y)
+print("x sample",x)
+
+# print(data)
+# print(y[0])
+# plt.plot(x[0],y)
+# plt.show()
+# Kersa utils? 
+# output_y = np_utils.to_categorical(y)
+# print(output_y)
+# print(myst[1])
+# print(myst)
+
+# files = glob("*.txt")
+# with open(files[0], "r") as f:
+#     data = [
+#         register.split()[1:]
+#         for register
+#         in f.readlines()
+#     ]
+
 
 # data = myst.ix[:,:-1]
 # print("Data",data)
-x = myst.data
-y = myst.data
+# x = myst.data
+# y = myst.data
 # features = myst.feature_names
 # n_features = len(features)
 
 # Plot pairs of variables
-plt.scatter(x[:,1], x[:,2], c = y, cmap=plt.cm.Set1, edgecolor='k')
-plt.xlabel(features[1])
-plt.ylabel(features[2])
-plt.show()
+# plt.scatter(x[:,1], x[:,2], c = y, cmap=plt.cm.Set1, edgecolor='k')
+# plt.xlabel(features[1])
+# plt.ylabel(features[2])
+# plt.show()
 
 # Train SVM classifier with all the available observations
 clf = svm.SVC(kernel = 'linear')
@@ -81,5 +131,5 @@ for train_index, test_index in kf.split(x):
 
     acc += acc_i 
 
-acc = acc/5
-print('ACC = ', acc)
+# acc = acc/5
+# print('ACC = ', acc)
