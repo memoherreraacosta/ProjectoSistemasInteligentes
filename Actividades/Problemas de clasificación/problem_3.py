adb156f57ec0569f023f7cb3d4892a1ba7425b55
RichiePalma <palmamendoza.5@hotmail.com>
2020-09-18 16:54:38 -0500

    Problem 3 from Problemas de clasificacion done
    Signed-off-by: RichiePalma <palmamendoza.5@hotmail.com>

--- Actividades/Problemas de clasificación/problem_3.py
+++ Actividades/Problemas de clasificación/problem_3.py
@@ -43,30 +43,35 @@ features = [str(i) for i in range(len(x[0]))]
 n_features = len(x[0])
 
 print("len x",len(x))
+print("shape x",x.shape)
 print("len y", len(y))
+print("Shape y", y.shape)
 # print(x)
 
-percentage = 0.1
+percentage = 0.7
 sample_size = round(len(x) * percentage)
 sample_indexes = np.random.choice(len(x),sample_size,replace=False)
 print("sample size",sample_size,"(",percentage*100,"% of total data )")
-print("indexes len",len(sample_indexes))
-print("indexes",sample_indexes)
+# print("indexes len",len(sample_indexes))
+# print("indexes",sample_indexes)
+# print("x with indexes",x[sample_indexes])
 
-temp_x = []
-temp_y = []
-for index in sample_indexes:
-    temp_x.append(x[index])
-    temp_y.append(y[index])
+x = x[sample_indexes]
+y = y[sample_indexes]
+# temp_x = []
+# temp_y = []
+# for index in sample_indexes:
+#     temp_x.append(x[index])
+#     temp_y.append(y[index])
 
 # print("temp x",temp_x[0])
 # print("temp y",temp_y)
 
-x = temp_x[:]
-y = temp_y[:]
+# x = temp_x[:]
+# y = temp_y[:]
 
-print("y sample",y)
-print("x sample",x)
+# print("y sample",y)
+# print("x sample",x)
 
 # print(data)
 # print(y[0])
@@ -95,17 +100,19 @@ print("x sample",x)
 # n_features = len(features)
 
 # Plot pairs of variables
-# plt.scatter(x[:,1], x[:,2], c = y, cmap=plt.cm.Set1, edgecolor='k')
-# plt.xlabel(features[1])
-# plt.ylabel(features[2])
-# plt.show()
+plt.scatter(x[:,1], x[:,2], c = y, cmap=plt.cm.Set1, edgecolor='k')
+plt.xlabel(features[1])
+plt.ylabel(features[2])
+plt.show()
 
 # Train SVM classifier with all the available observations
 clf = svm.SVC(kernel = 'linear')
 clf.fit(x, y)
 
 # Predict one new sample
-print("Prediction for a new observation", clf.predict( [[1.,2.,3.,4.]] ))
+shape = np.arange(x.shape[1])+1
+# print("shape x[1]",shape)
+print("Prediction for a new observation", clf.predict( [shape] ))
 
 # 5-fold cross-validation
 kf = KFold(n_splits=5, shuffle = True)
@@ -126,10 +133,10 @@ for train_index, test_index in kf.split(x):
 
     # Calculate confusion matrix and model performance
     cm = confusion_matrix(y_test, y_pred)
-    acc_i = (cm[0,0]+cm[1,1]+cm[2,2])/len(y_test)    
+    acc_i = (cm[0,0]+cm[1,1])/len(y_test)    
     print('acc = ', acc_i)
 
     acc += acc_i 
 
-# acc = acc/5
-# print('ACC = ', acc)
+acc = acc/5
+print('AVG ACC = ', acc)
