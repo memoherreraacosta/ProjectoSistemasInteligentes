9c0ee2b10a542528d8af62d53053fd50e08251f1
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-10-07 09:26:53 -0500

    Reorganizacion y avance problema 1 & 2
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

--- problema1.py
+++ problema1.py
@@ -2,158 +2,146 @@
 #   Sample program for data acquisition and recording.
 #------------------------------------------------------------------------------------------------------------------
 
-import socket
-import random
-
+#------------------------------------------------------------------------------------------------------------------
 import numpy as np
-import matplotlib.pyplot as plt
 
-from time import time
+from glob import glob
 from sklearn import svm
-from sklearn import datasets
-from matplotlib.mlab import psd
+from pprint import pprint
+from scipy.linalg import svd
+from keras.models import Sequential
+from keras.layers import Dense
 from sklearn.model_selection import KFold
 from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
 
-# Data configuration
-n_channels = 5
-samp_rate = 256
-emg_data = [[] for _ in range(n_channels)]
-samp_count = 0
 
-# Socket configuration
-UDP_IP = '127.0.0.1'
-UDP_PORT = 8000
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
-sock.settimeout(0.01)
+# Import Datos misteriosos data set
+data_set = glob("data/*")
+datos = np.loadtxt(data_set[0], dtype="float")
+x = datos[:,1:]
+y = datos[:,0]
+y = y-1
+n_features = len(x[0])
 
-# Data acquisition
-start_time = time()
 
-#Plot
-fig, axs = plt.subplots(2,2)
-fig.suptitle('Pairs of variables')
-
-# Train SVM classifier with all the available observations
+# 5-fold cross-validation
+# Evaluate model SVM linear
+kf = KFold(n_splits=5, shuffle = True)
 clf = svm.SVC(kernel = 'linear')
-
-ps = 0
 acc = 0
-precision1 = 0
-precision2 = 0
-precision3 = 0
-recall1 = 0
-recall2 = 0
-recall3 = 0
-while True:
-    try:
-        data, addr = sock.recvfrom(1024*1024)
+recall = np.array([0., 0.])
+for train_index, test_index in kf.split(x):
+    # Training phase
+    x_train = x[train_index, :]
+    y_train = y[train_index]
+    clf.fit(x_train, y_train)
 
-        values = np.frombuffer(data)
-        ns = len(values) // n_channels
-        samp_count += ns
-        ps += ns
+    # Test phase
+    x_test = x[test_index, :]
+    y_test = y[test_index]
+    y_pred = clf.predict(x_test)
 
-        for i in range(ns):
-            for j in range(n_channels):
-                emg_data[j].append(values[n_channels*i + j])
+    # Calculate confusion matrix and model performance
+    cm = confusion_matrix(y_test, y_pred)
+    acc_i = (cm[0,0]+cm[1,1])/len(y_test)
+    acc += acc_i
 
-        elapsed_time = time() - start_time
-        if (elapsed_time > 1):
-            window_data = np.array([x[samp_count-ps:] for x in emg_data])
-            # Power Spectral Analisis
-            power1, freq1 = psd(window_data[0], NFFT = ps, Fs = samp_rate)
-            power2, freq2 = psd(window_data[2], NFFT = ps, Fs = samp_rate)
+    recall[0] += cm[0,0]/ (cm[0,0]+cm[0,1])
+    recall[1] += cm[1,1]/ (cm[1,0]+cm[1,1])
 
-            axs[0,0].cla()
-            axs[0,1].cla()
-            axs[1,0].cla()
-            axs[1,1].cla()
-            start_time = time()
-            axs[0,0].plot(window_data[4], window_data[0], color = 'blue', label = 'Canal 1')
-            axs[0,1].plot(window_data[4], window_data[2], color = 'green', label = 'Canal 2')
-            axs[0,0].set(xlabel="Time(ms)", ylabel='micro V')
-            axs[0,0].set_ylim([-200, 200])
-            axs[0,1].set(xlabel="Time(ms)", ylabel='micro V')
-            axs[0,1].set_ylim([-200, 200])
+print("\nSVM Linear model")
+print('ACC SVM Linear = ', acc/5)
+print('RECALL SVM Linear = ', recall/5)
 
-            start_index = np.where(freq1 >= 4.0)[0][0]
-            end_index = np.where(freq1 >= 60.0)[0][0]
 
-            axs[1,0].plot(freq1[start_index:end_index], power1[start_index:end_index], color = "blue")
-            axs[1,0].set(xlabel='Hz', ylabel='Power')
+# 5-fold cross-validation
+# Evaluate model SVM linear
+kf = KFold(n_splits=5, shuffle = True)
+clf = svm.SVC(kernel = 'rbf')
+acc = 0
+recall = np.array([0., 0.])
+for train_index, test_index in kf.split(x):
+    # Training phase
+    x_train = x[train_index, :]
+    y_train = y[train_index]
+    clf.fit(x_train, y_train)
 
-            start_index = np.where(freq2 >= 4.0)[0][0]
-            end_index = np.where(freq2 >= 60.0)[0][0]
+    # Test phase
+    x_test = x[test_index, :]
+    y_test = y[test_index]
+    y_pred = clf.predict(x_test)
 
-            axs[1,1].plot(freq2[start_index:end_index], power2[start_index:end_index], color = 'green')
-            axs[1,1].set(xlabel='Hz', ylabel='Power')
+    # Calculate confusion matrix and model performance
+    cm = confusion_matrix(y_test, y_pred)
+    acc_i = (cm[0,0]+cm[1,1])/len(y_test)
+    acc += acc_i
 
-            plt.pause(.01)
+    recall[0] += cm[0,0]/ (cm[0,0]+cm[0,1])
+    recall[1] += cm[1,1]/ (cm[1,0]+cm[1,1])
 
-            #------------------------------------------------------------------------
-            # Train SVM classifier with all the available observations
-            # Falta acomodar x & y
-            x = x
-            y = y
-            #x = x.reshape(-1, 1)
-            clf.fit(x, y)
-            # Predict one new sample
-            new_ob = []
-            for i in range(7):
-                new_ob.append([random.randint(3,8)+random.random(),random.randint(2,4)+random.random(),random.randint(1,7)+random.random(),random.randint(0,2)+random.random()])
-            print("New Observation: " + str(new_ob))
-            print("Prediction for a new observation", clf.predict([[new_ob]]))
+print("\nSVM Radial model")
+print('ACC SVM Radial= ', acc/5)
+print('RECALL SVM Radial= ', recall/5)
 
-            # 5-fold cross-validation
-            kf = KFold(n_splits=10, shuffle = True)
-            clf = svm.SVC(kernel = 'linear')
-            for train_index, test_index in kf.split(x):
-                # Training phase
-                x_train = x[train_index, :]
-                y_train = y[train_index]
-                clf.fit(x_train, y_train)
 
-                # Test phase
-                x_test = x[test_index, :]
-                y_test = y[test_index]
-                y_pred = clf.predict(x_test)
+# Evaluate model Neuronal Network
+kf = KFold(n_splits=5, shuffle = True)
+acc = 0
+recall = np.array([0., 0.])
+for train_index, test_index in kf.split(x):
+    # Training phase
+    x_train = x[train_index, :]
+    y_train = y[train_index]
+    # y_train = np_utils.to_categorical(y_train)
 
-                # Calculate confusion matrix and model performance
-                cm = confusion_matrix(y_test, y_pred)
-                acc_i = (cm[0,0]+cm[1,1]+cm[2,2])/len(y_test)
-                # print('acc = ', acc_i)
-                acc += acc_i
+    clf = Sequential()
+    clf.add(Dense(8, input_dim=n_features, activation='relu'))
+    clf.add(Dense(8, activation='relu'))
+    clf.add(Dense(1, activation='sigmoid'))
+    clf.compile(loss='binary_crossentropy', optimizer='adam')
+    clf.fit(x_train, y_train, epochs=150, batch_size=8, verbose=0)
 
-                precision1_i = cm[0,0]/ (cm[0,0]+cm[1,0] + cm[2, 0])
-                precision2_i = cm[1,1]/ (cm[1,1]+cm[0,1] + cm[2, 1])
-                precision3_i = cm[2,2]/ (cm[2,2]+cm[1,2] + cm[0, 2])
-                precision1 += precision1_i
-                precision2 += precision2_i
-                precision3 += precision3_i
+    # Test phase
+    x_test = x[test_index, :]
+    y_test = y[test_index]
+    y_pred = (clf.predict(x_test) > 0.5).astype("int32")
 
-                recall1_i = cm[0,0]/ (cm[0,0]+cm[0,1] + cm[0, 2])
-                recall2_i = cm[1,1]/ (cm[1,0]+cm[1,1] + cm[1, 2])
-                recall3_i = cm[2,2]/ (cm[2,0]+cm[2,1] + cm[2, 2])
-                recall1 += recall1_i
-                recall2 += recall2_i
-                recall3 += recall3_i
+    cm = confusion_matrix(y_test, y_pred)
+    acc += (cm[0,0]+cm[1,1])/len(y_test)
+    recall[0] += cm[0,0]/(cm[0,0] + cm[0,1])
+    recall[1] += cm[1,1]/(cm[1,0] + cm[1,1])
 
-            print('ACC = ', acc/10)
-            print('PRECISION1 = ', precision1/10)
-            print('PRECISION2 = ', precision2/10)
-            print('PRECISION3 = ', precision3/10)
+print("\nNeuronal Network")
+acc = acc/5
+print('ACC Neuronal network= ', acc)
+recall = recall/5
+print('RECALL Neuronal network= ', recall)
 
-            print('RECALL1 = ', recall1/10)
-            print('RECALL2 = ', recall2/10)
-            print('RECALL3 = ', recall3/10)
 
-            print ("Muestras: ", ps)
-            print ("Cuenta: ", samp_count)
-            print("\n") # Double space
-            ps = 0
-    except socket.timeout:
-        pass
+results = dict()
+# 5-fold cross-validation
+kf = KFold(n_splits=5, shuffle = True)
+neighbours_distance = 5
+clf = KNeighborsClassifier(n_neighbors=neighbours_distance)
+acc = 0
+recall = np.array([0., 0.])
+for train_index, test_index in kf.split(x):
+    # Training phase
+    x_train = x[train_index, :]
+    y_train = y[train_index]
+    clf.fit(x_train, y_train)
+    # Test phase
+    x_test = x[test_index, :]
+    y_test = y[test_index]
+    y_pred = clf.predict(x_test)
+    # Calculate confusion matrix and model performance
+    cm = confusion_matrix(y_test, y_pred)
+    acc_i = (cm[0,0]+cm[1,1])/len(y_test)
+    acc += acc_i
+    recall[0] += cm[0,0]/ (cm[0,0]+cm[0,1])
+    recall[1] += cm[1,1]/ (cm[1,0]+cm[1,1])
 
-plt.show()
+print("\nKNN model")
+print("'K' Neighbors")
+pprint({"ACC": acc/5, "RECALL": recall/5})
